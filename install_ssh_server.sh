b59971b27907bc0b8cafd295c89303f1e4025bac
GIT-CLONE-GODA <125895032+GIT-CLONE-GODA@users.noreply.github.com>
2026-03-23 20:41:44 +0200

    Remove sudo from SSH server installation script

--- install_ssh_server.sh
+++ install_ssh_server.sh
@@ -7,14 +7,14 @@ PASSWORD=${1:-"kaggle"}  # Default password is "kaggle" if not provided
 echo "Setting up SSH with password: $PASSWORD"
 
 # Set root password
-echo "root:$PASSWORD" | sudo chpasswd
+echo "root:$PASSWORD" | chpasswd
 
 # Download ngrok (only if not already installed)
 if ! command -v ngrok &> /dev/null;
 then
     echo "ngrok not found. Downloading..."
     wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-    sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+    tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
     rm ngrok-v3-stable-linux-amd64.tgz
 else
     echo "ngrok is already installed."
@@ -22,18 +22,18 @@ fi
 
 # Install SSH-Server
 echo "Running apt update..."
-sudo apt update --allow-releaseinfo-change
+apt update --allow-releaseinfo-change
 
 echo "Installing OpenSSH server..."
-sudo apt install openssh-server -y
+apt install openssh-server -y
 
 # SSH Config - Enable password authentication
 echo "Configuring SSH..."
-echo "PermitRootLogin yes" | sudo tee -a /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-echo "PubkeyAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" | tee -a /etc/ssh/sshd_config
+echo "PubkeyAuthentication yes" | tee -a /etc/ssh/sshd_config
 
 echo "Restarting SSH service..."
-sudo service ssh restart
+service ssh restart
 
 echo "SSH Server configured successfully with password authentication!"
